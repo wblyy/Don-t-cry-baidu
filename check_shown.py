cf6ca0de5748cdd98345bb1ddd5504c41478be27
Balloon_Wang <wblyy0911@gmail.com>
2014-11-18 15:55:35 +0800

    check_shown_status
    查询数据库中shwon=0的qid，查看下数据结构并待下一步处理

---
+++ check_shown.py
@@ -0,0 +1,13 @@
+ # encoding: UTF-8
+import re
+import urllib2
+import urllib
+import time
+from mydb import Tiedb
+tiebadb = Tiedb()
+
+print tiebadb.is_q_shown_detected()
+
+
+#userNameEnc:"
+#",user:
