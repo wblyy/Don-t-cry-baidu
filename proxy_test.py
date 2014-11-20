40c2209f7878909d9eaf8d2058e88037a8a4393e
Balloon_Wang <wblyy0911@gmail.com>
2014-11-20 19:27:55 +0800

    冲突？

--- proxy_test.py
+++ proxy_test.py
@@ -20,7 +20,12 @@ proxy_handler_random = urllib2.ProxyHandler({"http":random.choice(proxy_dict)})
 
 proxy_auth_handler = urllib2.HTTPBasicAuthHandler()
 proxy_auth_handler.add_password('realm', 'host', 'username', 'password')
- 
+
+opener = urllib2.build_opener(proxy_handler, proxy_auth_handler) 
+f = opener.open('http://www.douban.com')
+content = f.read()
+print content
+
 opener = urllib2.build_opener(proxy_handler_random, proxy_auth_handler)
 # This time, rather than install the OpenerDirector, we use it directly:
 f = opener.open('http://www.douban.com')
