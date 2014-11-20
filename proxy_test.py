0cb4c8a53ce9642e0e25d5781c2f316069f9fefe
Balloon_Wang <wblyy0911@gmail.com>
2014-11-20 19:21:27 +0800

    random wrong position

--- proxy_test.py
+++ proxy_test.py
@@ -13,6 +13,7 @@ proxy_dict={'http': 'http://113.11.198.163:2223/',
 			'http': 'http://113.11.198.169:2223/'
 			}
 
+			
 proxy_handler = urllib2.ProxyHandler({'http': 'http://113.11.198.167:2223/'})
 #113.11.198.[163-169] 2223
 proxy_handler_random = urllib2.ProxyHandler({"http":random.choice(proxy_dict)})
@@ -20,7 +21,7 @@ proxy_handler_random = urllib2.ProxyHandler({"http":random.choice(proxy_dict)})
 proxy_auth_handler = urllib2.HTTPBasicAuthHandler()
 proxy_auth_handler.add_password('realm', 'host', 'username', 'password')
  
-opener = urllib2.build_opener(proxy_handler, proxy_auth_handler_random)
+opener = urllib2.build_opener(proxy_handler_random, proxy_auth_handler)
 # This time, rather than install the OpenerDirector, we use it directly:
 f = opener.open('http://www.douban.com')
 content = f.read()
