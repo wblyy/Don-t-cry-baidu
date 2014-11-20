f8c8138dbf1b40ac99a816df18a82b380cd93cd6
Balloon_Wang <wblyy0911@gmail.com>
2014-11-20 16:58:56 +0800

    AttributeError: 'module' object has no attribute 'get'

--- json.py
+++ json.py
@@ -1,7 +1,10 @@
 import requests
 import json
+import urllib2
+import urllib
 
-response = requests.get('http://54.223.153.21:8012/proxy')
+
+response = urllib2.urlopen('http://54.223.153.21:8012/proxy').read()#requests.get('http://54.223.153.21:8012/proxy')
 response.encoding = 'gb2312'
 test = json.loads(response)
 for a in test.keys():
