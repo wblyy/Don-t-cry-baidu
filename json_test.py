082be426f904df0d3308f71de8710a82a6e9613f
Balloon_Wang <wblyy0911@gmail.com>
2014-11-20 17:09:33 +0800

    AttributeError: 'module' object has no attribute 'loads'

--- json_test.py
+++ json_test.py
@@ -3,10 +3,9 @@ import requests
 import json
 import urllib2
 import urllib
-#AttributeError: 'module' object has no attribute 'loads'
 
-response = urllib2.urlopen('http://54.223.153.21:8012/proxy').read()#requests.get('http://54.223.153.21:8012/proxy')
-#response.decoding = 'gb2312'
+response = requests.get('http://54.223.153.21:8012/proxy')
+response.encoding = 'gb2312'
 test = json.loads(response)
 for a in test.keys():
     for b in test[a].keys():
