260028a36bec3955c53bd9d7146a86e5d2ed41ad
Balloon_Wang <wblyy0911@gmail.com>
2014-11-20 17:00:57 +0800

    AttributeError: 'str' object has no attribute 'decoding'

--- json.py
+++ json.py
@@ -5,7 +5,7 @@ import urllib
 
 
 response = urllib2.urlopen('http://54.223.153.21:8012/proxy').read()#requests.get('http://54.223.153.21:8012/proxy')
-response.decoding = 'gb2312'
+#response.decoding = 'gb2312'
 test = json.loads(response)
 for a in test.keys():
     for b in test[a].keys():
